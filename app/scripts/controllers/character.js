63b0171297b880a5eda89c3003eb53555874aa8f
Nathan Kluth <nlkluth@gmail.com>
2014-02-22 17:02:29 -0700

    trying less scope assignment

--- app/scripts/controllers/character.js
+++ app/scripts/controllers/character.js
@@ -10,27 +10,8 @@ controllers.controller('CharacterController', ['$scope', '$routeParams', 'Charac
     };
 
     Character.info($scope.character.realm, $scope.character.name).then(function(result) {
-      $scope.character.achievements = result.data.achievementPoints;
-      $scope.character.battlegroup = result.data.battlegroup;
-      $scope.character.class = result.data.class;
-      $scope.character.gender = result.data.gender;
-      $scope.character.level = result.data.level;
-      $scope.character.race = result.data.race;
-      $scope.character.thumbnail = result.data.thumbnail;
-      $scope.character.totalHonerableKills = result.data.totalHonerableKills;
-
-      $scope.achievementsCompleted = result.data.achievements.achievementsCompleted;
-      $scope.feed = result.data.feed;
-      $scope.guild = result.data.guild;
-      $scope.items = result.data.items;
-      $scope.mounts = result.data.mounts.collected;
-      $scope.pets = result.data.pets.collected;
-      $scope.professions = result.data.professions;
-      $scope.progression = result.data.progression.raids;
-      $scope.pvp = result.data.pvp.brackets;
-      $scope.quests = result.data.quests;
-      $scope.stats = result.data.stats;
-      $scope.talents = result.data.talents[0];
+      console.log(result.data);
+      $scope.info = result.data;
     }, function(error) {
       console.log(error, 'error');
     });
