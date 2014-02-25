a80754444744e4345ff1ef0dae7adaaa2327b7d5
Nathan Kluth <nlkluth@gmail.com>
2014-02-25 12:47:06 -0700

    added guild service reference to controller

--- app/scripts/controllers/guild.js
+++ app/scripts/controllers/guild.js
@@ -1,7 +1,14 @@
 'use strict';
 
-var nav = angular.module('wow.controller.guild', []);
+var nav = angular.module('wow.controller.guild', ['wow.services.guild']);
 
-nav.controller('GuildController', ['$scope', '$stateParams', function($scope, $stateParams) {
+nav.controller('GuildController', ['$scope', '$stateParams', 'Guild', function($scope, $stateParams, Guild) {
   $scope.name = $stateParams.name;
+  $scope.realm = $stateParams.realm;
+
+  Guild.info($scope.realm, $scope.name).then(function(result) {
+      $scope.info = result.data;
+    }, function(error) {
+      console.log(error, 'error');
+    });
 }]);
