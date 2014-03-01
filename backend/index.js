0256047f6b6cebed45f5ba36edf13ac3ac9e1eee
Nathan Kluth <nlkluth@gmail.com>
2014-03-01 10:57:33 -0700

    added item route to backend

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,15 @@ var port = process.env.PORT || 3000;
 var realms = require('./lib/realms'),
   character = require('./lib/character'),
   guild = require('./lib/guild'),
-  quest = require('./lib/quests');
+  quest = require('./lib/quests'),
+  item = require('./lib/items');
 
 api.get('/api/realms', realms.list);
 api.get('/api/pvp/:bracket', character.list);
 api.get('/api/character/:realm/:name', character.detail);
 api.get('/api/guild/:realm/:name', guild.detail);
 api.get('/api/quest/:id', quest.detail);
+api.get('/api/item/:itemId', item.detail);
 
 api.listen(port);
 debug('Listening on %d', port);
