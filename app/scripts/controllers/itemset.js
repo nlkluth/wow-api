eacd043b5aa9496c612742c9d7e34b1f86bf6365
Nathan Kluth <nlkluth@gmail.com>
2014-03-03 12:51:49 -0700

    controller & stubview

---
+++ app/scripts/controllers/itemset.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var controllers = angular.module('wow.controller.itemset', ['wow.services.item']);
+
+controllers.controller('ItemController', ['$scope', '$stateParams', 'Item',
+  function($scope, $stateParams, Item) {
+    $scope.id = $stateParams.id;
+    Item.setinfo($scope.id).then(function(result) {
+      $scope.info = result.data;
+    }, function(error) {
+      console.log(error, 'error');
+    });
+  }]);
