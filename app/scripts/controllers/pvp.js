0587a53f10a321b266d021954e5a1f699c6230b1
Nathan Kluth <nlkluth@gmail.com>
2014-03-06 11:28:18 -0700

    filter search on pvp list

--- app/scripts/controllers/pvp.js
+++ app/scripts/controllers/pvp.js
@@ -5,6 +5,9 @@ var controllers = angular.module('wow.controller.pvp', ['wow.services.pvp']);
 controllers.controller('PvPController', ['$scope', '$stateParams', 'PvP',
   function($scope, $stateParams, PvP) {
     $scope.bracket = '3v3';
+    $scope.searchsize = {
+      'limit': 10
+    };
 
     PvP.list($scope.bracket).then(function(result) {
       $scope.leaderboard = result.data.rows;
