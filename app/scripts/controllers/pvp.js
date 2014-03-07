001b15f68a089504ab1294cb6d2f00a3edc360be
Nathan Kluth <nlkluth@gmail.com>
2014-03-07 11:15:13 -0700

    starting to mess with pagination

--- app/scripts/controllers/pvp.js
+++ app/scripts/controllers/pvp.js
@@ -9,8 +9,13 @@ controllers.controller('PvPController', ['$scope', '$stateParams', 'PvP',
       'limit': 10
     };
 
+    $scope.setPage = function (pageNo) {
+      $scope.currentPage = pageNo;
+    };
+
     PvP.list($scope.bracket).then(function(result) {
       $scope.leaderboard = result.data.rows;
+      $scope.totalItems = result.data.rows.length;
     }, function(error) {
       console.log(error, 'error');
     });
