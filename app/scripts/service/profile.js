c563772ae3602c04e65d4f6c3b3980786441c716
Nathan Kluth <nlkluth@gmail.com>
2014-03-11 09:33:29 -0600

    profile service

---
+++ app/scripts/service/profile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var services = angular.module('wow.services.profile', []);
+
+services.factory('Profile', ['$http', function($http) {
+  function load(path) {
+    var request = $http({
+      method: 'GET',
+      url: path,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+
+    return request;
+  }
+
+  return {
+    list: function() {
+      return load('/api/profiles');
+    }
+  };
+}]);
